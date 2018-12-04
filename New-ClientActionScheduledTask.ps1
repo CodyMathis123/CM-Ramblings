acb4022089d99ba2b5438fa5bda34258b920086a
CodyMathis123 <cody.mathis123@gmail.com>
2018-12-04 07:52:52 -0500

    Powershell 2.0 / Win 7 compatible
    Now compatible with Powershell 2.0 and compatible with Windows 7.
    
    Generating one file that has the function and passing the desired schedules to it.

--- New-ClientActionScheduledTask.ps1
+++ New-ClientActionScheduledTask.ps1
@@ -6,19 +6,19 @@
     Script will create a scheduled task with the specified duration and recurrance interval to invoke CM Client Actions on a machine. This is to help expedite a machine receiving all needed policies/applications/updates from SCCM.
 
 .PARAMETER Duration
-	Specify the duration (in hours) for the task. This is how long the duration will recur for. Note: The scheduled task deletes itself after this period (1-24)
+	Specify the duration (in hours) for the task. This is how long the duration will recur for. Note: The scheduled task deletes itself after this period
 
 .PARAMETER Interval
-	Specifies the interval to run the actions at in minutes - this is the interval for scheduled task recurrence (1-60)
+	Specifies the interval to run the actions at in minutes - this is the interval for scheduled task recurrence
 
 .PARAMETER Schedule
 	Specifies th schedules to run - 'HardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval'
 
 .PARAMETER TaskName
-	Set the task name - defaults to 'SCCM Expeditious Response Task'
-
+    Set the task name - defaults to "SCCM Action Scheduler - [$($Schedule -join ',')]"
+    
 .PARAMETER FileName
-    Sets the file name of the script that is generated. This allows you to run the script multiple times to assign differenct actions to differnt schedules
+	Set the file name - defaults to "Start-CMClientAction.ps1" - this end up in c:\windows\temp so it'll eventually be deleted. 
 
 .EXAMPLE
 	# Creates a scheduled task to run a MachinePol ad HardwareInv every 30 minutes for 24 hours.
@@ -29,35 +29,40 @@
     Author:      Cody Mathis
     Contact:     @CodyMathis123
     Created:     11-29-2018
-    Updated:     12-02-2018
+    Updated:     12-04-2018
 #>
 param (
     [parameter(Mandatory = $false)]
-    [ValidateRange(1, 24)]
     [int]$Duration = 24,
     [parameter(Mandatory = $false)]
-    [ValidateRange(1, 60)]
     [int]$Interval = 30,
     [parameter(Mandatory = $false)]
     [ValidateSet('HardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval')]
     [string[]]$Schedule,
     [parameter(Mandatory = $false)]
-    [string]$TaskName = "SCCM Expeditious Response Task",
+    [string]$TaskName,
     [parameter(Mandatory = $false)]
-    [string]$FileName = "ClientActionScheduledTask.ps1"
+    [string]$FileName = 'Start-CMClientAction.ps1'
 )
 $ErrorActionPreference = 'Stop'
+if (-not $PSBoundParameters.ContainsKey('TaskName')) {
+    $TaskName = "SCCM Action Scheduler - [$($Schedule -join ',')]"
+} 
+
+if (-not $FileName.EndsWith('.ps1')) {
+    $FileName = [string]::Format("{0}.ps1", $FileName)
+}
+
 # I am using $env:SystemRoot\temp because $env:temp will be the users temp folder if this is not run as system, and our scheduled task runs as system and may have access issues to the users temp folder from a scheduled task
 $File = Join-Path -Path "$env:SystemRoot\temp" -ChildPath $FileName
 
-#region generate file which the scheduled task will execute
-@"
+#region functions
 <#
 .SYNOPSIS
     Invokes CM Client actions on local or remote machines
 
 .DESCRIPTION
-    This script will allow you to invoke a set of CM Client actions on a machine (with optional credentials), providing a list of the actions and an optional delay betweens actions.
+    This script will allow you to invoke a set of CM Client actions on a machine (with optional credentials), providing a list of the actions and an optional delay betweens actions. 
     The function will attempt for 5 minutes to invoke the action, with a 10 second delay inbetween attempts. This is to account for invoke-wmimethod failures.
 
 .PARAMETER Schedule
@@ -74,106 +79,155 @@ $File = Join-Path -Path "$env:SystemRoot\temp" -ChildPath $FileName
 
 .EXAMPLE
 	# Start a machine policy eval and a hardware inventory cycle
-	.\Start-CMClientAction.ps1 -Schedule MachinePol,HardwareInv
+	Start-CMClientAction -Schedule MachinePol,HardwareInv
 
 .NOTES
+    FileName:    Start-CMClientAction.ps1
     Author:      Cody Mathis
     Contact:     @CodyMathis123
     Created:     11-29-2018
     Updated:     11-29-2018
 #>
 function Start-CMClientAction {
-    [CmdletBinding(SupportsShouldProcess = `$true)]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param
     (
-        [parameter(Mandatory = `$true)]
+        [parameter(Mandatory = $true)]
         [ValidateSet('HardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval')]
-        [string[]]`$Schedule,
-        [parameter(Mandatory = `$false)]
+        [string[]]$Schedule,
+        [parameter(Mandatory = $false)]
         [ValidateRange(0, 30)]
-        [int]`$Delay = 5,
-        [parameter(Mandatory = `$false)]
-        [string[]]`$ComputerName = `$env:COMPUTERNAME,
-        [parameter(Mandatory = `$false)]
-        [pscredential]`$Credential
+        [int]$Delay = 5,
+        [parameter(Mandatory = $false)]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+        [parameter(Mandatory = $false)]
+        [pscredential]$Credential
     )
     begin {
-        `$TimeSpan = New-TimeSpan -Minutes 5
+        $TimeSpan = New-TimeSpan -Minutes 5
     }
     process {
-        foreach (`$Computer in `$ComputerName) {
-            foreach (`$Option in `$Schedule) {
-                `$Action = switch (`$Option) {
-                    'HardwareInv' {
+        foreach ($Computer in $ComputerName) {
+            foreach ($Option in $Schedule) {
+                $Action = switch ($Option) {
+                    HardwareInv {
                         "{00000000-0000-0000-0000-000000000001}"
                     }
-                    'SoftwareInv' {
+                    SoftwareInv {
                         "{00000000-0000-0000-0000-000000000002}"
                     }
-                    'UpdateScan' {
+                    UpdateScan {
                         "{00000000-0000-0000-0000-000000000113}"
                     }
-                    'UpdateEval' {
+                    UpdateEval {
                         "{00000000-0000-0000-0000-000000000108}"
                     }
-                    'MachinePol' {
+                    MachinePol {
                         "{00000000-0000-0000-0000-000000000021}"
                     }
-                    'AppEval' {
+                    AppEval {
                         "{00000000-0000-0000-0000-000000000121}"
                     }
                 }
 
-                `$StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+                $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
                 do {
                     try {
-                        `$invokeWmiMethodSplat = @{
-                            ComputerName = `$Computer
+                        $invokeWmiMethodSplat = @{
+                            ComputerName = $Computer
                             Name         = 'TriggerSchedule'
                             Namespace    = 'root\ccm'
                             Class        = 'sms_client'
-                            ArgumentList = `$Action
+                            ArgumentList = $Action
                         }
-                        if (`$PSBoundParameters.ContainsKey('Credential')) {
-                            `$invokeWmiMethodSplat.Add('Credential', `$Credential)
+                        if ($PSBoundParameters.ContainsKey('Credential')) {
+                            $invokeWmiMethodSplat.Add('Credential', $Credential)
                         }
-                        Write-Verbose "Triggering a `$Schedule Cycle on `$Computer via the 'TriggerSchedule' WMI method"
-                        `$Invocation = Invoke-WmiMethod @invokeWmiMethodSplat
+                        Write-Verbose "Triggering a $Schedule Cycle on $Computer via the 'TriggerSchedule' WMI method"
+                        $Invocation = Invoke-WmiMethod @invokeWmiMethodSplat
                     }
                     catch {
-                        Write-Error "Failed to invoke the `$Schedule cycle via WMI. Will retry every 10 seconds until [StopWatch `$(`$StopWatch.Elapsed) -ge 5 minutes] Error: `$(`$_.Exception.Message)"
+                        Write-Error "Failed to invoke the $Schedule cycle via WMI. Will retry every 10 seconds until [StopWatch $($StopWatch.Elapsed) -ge 5 minutes] Error: $($_.Exception.Message)"
                         Start-Sleep -Seconds 10
                     }
                 }
-                until (`$Invocation -or `$StopWatch.Elapsed -ge `$TimeSpan)
-                if (`$Invocation) {
-                    Write-Verbose "Successfully invoked the `$Schedule Cycle on `$Computer via the 'TriggerSchedule' WMI method"
-                    Start-Sleep -Seconds `$Delay
+                until ($Invocation -or $StopWatch.Elapsed -ge $TimeSpan)
+                if ($Invocation) {
+                    Write-Verbose "Successfully invoked the $Schedule Cycle on $Computer via the 'TriggerSchedule' WMI method"
+                    Start-Sleep -Seconds $Delay
                 }
-                `$StopWatch.Reset()
+                $StopWatch.Reset()    
             }
         }
     }
     end {
-        Write-Verbose "Following actions invoked - `$Schedule"
+        Write-Verbose "Following actions invoked - $Schedule"
     }
 }
 
-Start-CMClientAction -Schedule $($Schedule -join ',')
-"@  | Out-File -FilePath $File -Force
+function New-ScheduledTaskTimeString {
+    param(
+        [Parameter(Mandatory = $false)]
+        [int]$Hours = 0,
+        [Parameter(Mandatory = $false)]
+        [int]$Minutes = 0
+    )
+    $TimeSpan = New-TimeSpan -Hours $Hours -Minutes $Minutes
+    $TimeSpanDays = $TimeSpan | Select-Object -ExpandProperty Days
+    $TimeSpanHours = $TimeSpan | Select-Object -ExpandProperty Hours
+    $TimeSpanMinutes = $TimeSpan | Select-Object -ExpandProperty Minutes
+
+    if ($TimeSpanDays -gt 0) {
+        $OutputDays = [string]::Format("{0}D", $TimeSpanDays)
+    }
+
+    if ($TimeSpanHours -gt 0) {
+        $OutputHours = [string]::Format("{0}H", $TimeSpanHours)
+    }
+
+    if ($TimeSpanMinutes -gt 0) {
+        $OutputMinutes = [string]::Format("{0}M", $TimeSpanMinutes)
+    }
+
+    [string]::Format("P{0}T{1}{2}", $OutputDays, $OutputHours, $OutputMinutes)
+
+}
+#endregion functions
+
+#region generate file which the scheduled task will execute
+${Function:Start-CMClientAction}  | Out-File -FilePath $File -Force
 #endregion generate file which the scheduled task will execute
 
 #region create and register a scheduled task with expiration and set to delete itself
 try {
-    $TaskAction = New-ScheduledTaskAction -Execute "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File `"$File`""
-    $TaskSettings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -Compatibility Win8 -AllowStartIfOnBatteries:$true -RunOnlyIfIdle:$false
-    $TaskTrigger = New-ScheduledTaskTrigger -Once -At ($Start = (Get-Date).AddSeconds(30)) -RepetitionInterval (New-TimeSpan -Minutes $Interval) -RepetitionDuration (New-TimeSpan -Hours $Duration)
-    $Task = New-ScheduledTask -Action $TaskAction -Settings $TaskSettings -Trigger $TaskTrigger
-    Register-ScheduledTask -InputObject $Task -TaskName $TaskName -User "NT AUTHORITY\SYSTEM" | Out-Null
-    $CreatedTask = Get-ScheduledTask -TaskName $TaskName
-    $CreatedTask.Triggers[0].EndBoundary = $Start.AddHours($Duration).AddMinutes(5).ToString('s')
+    $TScomObject = New-Object -ComObject ("Schedule.Service")
+    $TScomObject.Connect()
+    $RootTSFolder = $TScomObject.GetFolder("\")
+    $TaskDefinition = $TScomObject.NewTask(0)
+    $TaskDefinition.Settings.Enabled = $true
+    $TaskDefinition.Settings.AllowDemandStart = $true
+    $TaskDefinition.Settings.DisallowStartIfOnBatteries = $false
+    $TaskDefinition.Settings.DeleteExpiredTaskAfter = "PT0S"
+    
+    #region task trigger creation
+    $Start = (Get-Date).AddSeconds(30)
+    $CalculatedDuration = New-ScheduledTaskTimeString -Hours $Duration
+    $CalculatedInterval = New-ScheduledTaskTimeString -Minutes $Interval
+    $triggers = $TaskDefinition.Triggers
+    $trigger = $triggers.Create(1)
+    $trigger.StartBoundary = $Start.ToString("yyyy-MM-dd'T'HH:mm:ss")
+    $trigger.Repetition.Duration = $CalculatedDuration
+    $trigger.Repetition.Interval = $CalculatedInterval
+    $trigger.Repetition.StopAtDurationEnd = $true
+    $trigger.EndBoundary = $Start.AddHours($Duration).AddMinutes(5).ToString('s')
+    $trigger.Enabled = $true
+    #endregion task trigger creation
+
+    $Action = $TaskDefinition.Actions.Create(0)
+    $Action.Path = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+    $Action.Arguments = "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command `"$File`" -Schedule $($Schedule -join ',')"
+    $RootTSFolder.RegisterTaskDefinition($TaskName, $TaskDefinition, 6, "System", $null, 5)
     $CreatedTask.Settings.DeleteExpiredTaskAfter = "PT0S"
-    Set-ScheduledTask -InputObject $CreatedTask | Out-Null
 }
 catch {
     Write-Error "Failed to create the scheduled task - $($_.Exception.Message)"

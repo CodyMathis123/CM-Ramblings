a5379989ecb6b2f4c18d1b98053ca9b4251962ed
CodyMathis123 <cody.mathis123@gmail.com>
2019-02-26 09:59:20 -0500

    add FullHardwareInv

--- New-ClientActionScheduledTask.ps1
+++ New-ClientActionScheduledTask.ps1
@@ -66,7 +66,8 @@ if (-not $FileName.EndsWith('.ps1')) {
 $File = Join-Path -Path "$env:SystemRoot\temp" -ChildPath $FileName
 
 #region functions
-<#
+function Start-CMClientAction {
+    <#
 .SYNOPSIS
     Invokes CM Client actions on local or remote machines
 
@@ -75,13 +76,16 @@ $File = Join-Path -Path "$env:SystemRoot\temp" -ChildPath $FileName
     The function will attempt for 5 minutes to invoke the action, with a 10 second delay inbetween attempts. This is to account for invoke-wmimethod failures.
 
 .PARAMETER Schedule
-	Define the schedules to run on the machine - 'HardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval', 'DDR'
+	Define the schedules to run on the machine - 'HardwareInv', 'FullHardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval'
 
 .PARAMETER Delay
 	Specify the delay in seconds between each schedule when more than one is ran - 0-30 seconds
 
 .PARAMETER ComputerName
-	Specifies the computers to run this against
+    Specifies the computers to run this against
+    
+.PARAMETER Timeout
+    Specifies the timeout in minutes after which any individual computer will stop attempting the schedules. Default is 5 minutes.
 
 .PARAMETER Credential
 	Optional PSCredential
@@ -97,23 +101,25 @@ $File = Join-Path -Path "$env:SystemRoot\temp" -ChildPath $FileName
     Created:     11-29-2018
     Updated:     12-23-2018
 #>
-function Start-CMClientAction {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param
     (
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+        [Alias('Computer', 'HostName', 'ServerName', 'IPAddress')]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
         [parameter(Mandatory = $true)]
-        [ValidateSet('HardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval', 'DDR')]
+        [ValidateSet('HardwareInv', 'FullHardwareInv', 'SoftwareInv', 'UpdateScan', 'UpdateEval', 'MachinePol', 'AppEval', 'DDR')]
         [string[]]$Schedule,
         [parameter(Mandatory = $false)]
         [ValidateRange(0, 30)]
         [int]$Delay = 5,
         [parameter(Mandatory = $false)]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
+        [int]$Timeout = 5,
         [parameter(Mandatory = $false)]
         [pscredential]$Credential
     )
     begin {
-        $TimeSpan = New-TimeSpan -Minutes 5
+        $TimeSpan = New-TimeSpan -Minutes $Timeout
         # Load Microsoft.SMS.TSEnvironment COM object
         try {
             $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction SilentlyContinue
@@ -131,37 +137,64 @@ function Start-CMClientAction {
             foreach ($Option in $Schedule) {
                 $Action = switch ($Option) {
                     HardwareInv {
-                        "{00000000-0000-0000-0000-000000000001}"
+                        '{00000000-0000-0000-0000-000000000001}'
+                    }
+                    FullHardwareInv {
+                        '{00000000-0000-0000-0000-000000000001}'
                     }
                     SoftwareInv {
-                        "{00000000-0000-0000-0000-000000000002}"
+                        '{00000000-0000-0000-0000-000000000002}'
                     }
                     UpdateScan {
-                        "{00000000-0000-0000-0000-000000000113}"
+                        '{00000000-0000-0000-0000-000000000113}'
                     }
                     UpdateEval {
-                        "{00000000-0000-0000-0000-000000000108}"
+                        '{00000000-0000-0000-0000-000000000108}'
                     }
                     MachinePol {
-                        "{00000000-0000-0000-0000-000000000021}"
+                        '{00000000-0000-0000-0000-000000000021}'
                     }
                     AppEval {
-                        "{00000000-0000-0000-0000-000000000121}"
+                        '{00000000-0000-0000-0000-000000000121}'
                     }
                     DDR {
-                        "{00000000-0000-0000-0000-000000000003}"
+                        '{00000000-0000-0000-0000-000000000003}'
                     }
                 }
 
                 $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
                 do {
                     try {
+                        Remove-Variable MustExit -ErrorAction SilentlyContinue
+                        Remove-Variable Invocation -ErrorAction SilentlyContinue
+                        if ($Option -eq 'FullHardwareInv') {
+                            $getWMIObjectSplat = @{
+                                ComputerName = $Computer
+                                Namespace    = 'root\ccm\invagt'
+                                Class        = 'InventoryActionStatus'
+                                Filter       = "InventoryActionID ='$Action'"
+                                ErrorAction  = 'Stop'
+                            }
+                            if ($PSBoundParameters.ContainsKey('Credential')) {
+                                $getWMIObjectSplat.Add('Credential', $Credential)
+                            }
+                            Write-Verbose "Attempting to delete Hardware Inventory history for $Computer as a FullHardwareInv was requested"
+                            $HWInv = Get-WMIObject @getWMIObjectSplat
+                            if ($null -ne $HWInv) {
+                                $HWInv.Delete()
+                                Write-Verbose "Hardware Inventory history deleted for $Computer"
+                            }
+                            else {
+                                Write-Verbose "No Hardware Inventory history to delete for $Computer"
+                            }
+                        }
                         $invokeWmiMethodSplat = @{
                             ComputerName = $Computer
                             Name         = 'TriggerSchedule'
                             Namespace    = 'root\ccm'
                             Class        = 'sms_client'
                             ArgumentList = $Action
+                            ErrorAction  = 'Stop'
                         }
                         if ($PSBoundParameters.ContainsKey('Credential')) {
                             $invokeWmiMethodSplat.Add('Credential', $Credential)
@@ -169,15 +202,22 @@ function Start-CMClientAction {
                         Write-Verbose "Triggering a $Option Cycle on $Computer via the 'TriggerSchedule' WMI method"
                         $Invocation = Invoke-WmiMethod @invokeWmiMethodSplat
                     }
+                    catch [System.UnauthorizedAccessException] {
+                        Write-Error -Message "Access denied to $Computer" -Category AuthenticationError -Exception $_.Exception
+                        $MustExit = $true
+                    }
                     catch {
-                        Write-Error "Failed to invoke the $Option cycle via WMI. Will retry every 10 seconds until [StopWatch $($StopWatch.Elapsed) -ge 5 minutes] Error: $($_.Exception.Message)"
+                        Write-Warning "Failed to invoke the $Option cycle via WMI. Will retry every 10 seconds until [StopWatch $($StopWatch.Elapsed) -ge 5 minutes] Error: $($_.Exception.Message)"
                         Start-Sleep -Seconds 10
                     }
                 }
-                until ($Invocation -or $StopWatch.Elapsed -ge $TimeSpan)
+                until ($Invocation -or $StopWatch.Elapsed -ge $TimeSpan -or $MustExit)
                 if ($Invocation) {
                     Write-Verbose "Successfully invoked the $Option Cycle on $Computer via the 'TriggerSchedule' WMI method"
                     Start-Sleep -Seconds $Delay
+                }
+                elseif ($StopWatch.Elapsed -ge $TimeSpan) {
+                    Write-Error "Failed to invoke $Option cycle via WMI after 5 minutes of retrrying."
                 }
                 $StopWatch.Reset()    
             }

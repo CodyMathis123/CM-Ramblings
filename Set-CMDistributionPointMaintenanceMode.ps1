cfb86f56a716d21c007910512594b7aa90a7b430
CodyMathis123 <cody.mathis123@gmail.com>
2019-06-08 14:11:34 -0400

    Add help, change to verbose, support whatif

--- Set-CMDistributionPointMaintenanceMode.ps1
+++ Set-CMDistributionPointMaintenanceMode.ps1
@@ -1,4 +1,23 @@
+<#
+.SYNOPSIS
+    Allows you to toggle maintenance mode on a distribution point
+.DESCRIPTION
+    This function allows you to select a distribution point and set whether the 'Maintenance Mode' feature introduced in ConfigMgr 1902
+        is toggled on or off.
+.PARAMETER SMSProvider
+    Define the SMS Provider which the WMI queries will execute against.
+.PARAMETER DistributionPoint
+    Provides the Distribution Point which you want to change the maintenance mode of. This should be provided as a FQDN,
+        but if you provide the shortname we will also attempt a search with a wildcard.
+.PARAMETER MaintenanceMode
+    The desired state of of Maintenance Mode for the distribution point. This is either 'On' or 'Off'
+.EXAMPLE
+    C:\PS> Set-CMDistributionPointMaintenanceMode -SMSProvider SCCM.CONTOSO.COM -DistributionPoint DP.CONTOSO.COM -MaintenanceMode On
+.NOTES
+    The account you run this as must have the proper permissions to perform the maintenance mode action
+#>
 function Set-CMDistributionPointMaintenanceMode {
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param(
         [parameter(Mandatory = $true)]
         [string]$SMSProvider,
@@ -28,7 +47,7 @@ function Set-CMDistributionPointMaintenanceMode {
                 Write-Error "WMI query for a distribution point succeded, but no object was returned. [Filter = `"$Filter`"] against [SMSProvider=$SMSProvider]" -ErrorAction Stop
             }
         }
-        Write-Output "Identified Distribution point with [NALPath=$($DP.NALPath)]"
+        Write-Verbose "Identified Distribution point with [NALPath=$($DP.NALPath)]"
     }
     catch {
         Write-Error "Failed to query for a distribution point with [Filter `"$Filter`"] against [SMSProvider=$SMSProvider]" -ErrorAction Stop
@@ -54,12 +73,14 @@ function Set-CMDistributionPointMaintenanceMode {
                 Mode    = [uint32]$Mode
             }
         }
-        $Return = Invoke-CimMethod @invokeCimMethodSplat
-        if ($Return.ReturnValue -ne 0) {
-            Write-Error "Failed to set [DistributionPoint=$DistributionPoint] [MaintenanceMode=$MaintenanceMode] against [SMSProvider=$SMSProvider]" -ErrorAction Stop
-        }
-        elseif ($Return.ReturnValue -eq 0) {
-            Write-Output "Set [DistributionPoint=$DistributionPoint] [MaintenanceMode=$MaintenanceMode]"
+        if ($PSCmdlet.ShouldProcess($DistributionPoint, "MaintenanceMode $MaintenanceMode")) {
+            $Return = Invoke-CimMethod @invokeCimMethodSplat
+            if ($Return.ReturnValue -ne 0) {
+                Write-Error "Failed to set [DistributionPoint=$DistributionPoint] [MaintenanceMode=$MaintenanceMode] against [SMSProvider=$SMSProvider]" -ErrorAction Stop
+            }
+            elseif ($Return.ReturnValue -eq 0) {
+                Write-Verbose "Set [DistributionPoint=$DistributionPoint] [MaintenanceMode=$MaintenanceMode]"
+            }
         }
     }
     catch {

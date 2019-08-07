cab23bfcf2b463ffa793d835d5e10850dfc300ee
CodyMathis123 <cody.mathis123@gmail.com>
2019-08-07 08:53:19 -0400

    add notes

--- Get-HinvFrequency.sql
+++ Get-HinvFrequency.sql
@@ -1,8 +1,13 @@
+/*
+	This query will show you the count of records per Hardware Inventory class in your HinvChangeLog table.
+	This can help you identify a class that is create a lot of hardware inventory 'traffic' and potentially bloating
+	your database.
+*/
 SELECT map.DisplayName 
 	, map.InvClassName AS 'Inventory View'
-	, COUNT(HINV.RecordID) 
+	, COUNT(HINV.RecordID)
 FROM HinvChangeLog hinv
-	LEFT JOIN v_GroupMap map ON map.GroupID = hinv.GroupKey
+    LEFT JOIN v_GroupMap map ON map.GroupID = hinv.GroupKey
 GROUP BY map.DisplayName
 	, map.InvClassName
 ORDER BY 3 DESC

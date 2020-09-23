449cd287c2ec29f1e29390f36d79e765269186c3
Cody Mathis <cody@patchmypc.com>
2020-09-23 08:08:36 -0400

    use v_StateNames

--- ConfigMgrSUP_LastScan_Summary.sql
+++ ConfigMgrSUP_LastScan_Summary.sql
@@ -1,23 +1,16 @@
-Select v_R_System.name0 as 'Name'
-        , v_R_System.ResourceID
-       , v_RA_System_ResourceNames.Resource_Names0 as 'FQDN'
-       , v_R_System.Resource_Domain_OR_Workgr0 as 'DOMAIN'
-       , v_GS_OPERATING_SYSTEM.Caption0 as 'Operating System'
-       , v_UpdateScanStatus.LastScanTime
-       , v_UpdateScanStatus.LastErrorCode
-       , v_GS_WORKSTATION_STATUS.LastHWScan as 'Last Hardware Scan'
-       ,  CASE v_UpdateScanStatus.LastScanState
-              When '0' THEN 'SCAN STATE UNKNOWN'
-              when '1' THEN 'Scan is waiting for catalog location'
-              when '2' then 'Scan is running'
-              when '3' then 'Scan is completed'
-              when '4' then 'Scan is pending retry' 
-              when '5' then 'Scan failed'
-              when '6' then 'Scan completed with errors'
-              else 'broken'
-         end as 'Last Scan State'
-from v_R_System
-full join v_RA_System_ResourceNames on v_R_System.ResourceID = v_RA_System_ResourceNames.ResourceID
-Full join v_UpdateScanStatus on v_R_System.ResourceID = v_UpdateScanStatus.ResourceID
-full join v_GS_WORKSTATION_STATUS on v_R_System.ResourceID = v_GS_WORKSTATION_STATUS.ResourceID
-Full Join v_GS_OPERATING_SYSTEM on v_R_System.ResourceID = v_GS_OPERATING_SYSTEM.ResourceID
+SELECT s.Netbios_Name0 AS 'Name'
+	, s.ResourceID
+	, srn.Resource_Names0 AS 'FQDN'
+	, s.Resource_Domain_OR_Workgr0 AS 'DOMAIN'
+	, v_GS_OPERATING_SYSTEM.Caption0 AS 'Operating System'
+	, sn.StateDescription AS 'LastScanState'
+	, scan.LastScanTime
+	, scan.LastErrorCode
+	, scan. LastScanPackageLocation
+	, wsStatus.LastHWScan AS 'Last Hardware Scan'	
+FROM v_R_System s
+JOIN v_RA_System_ResourceNames srn ON s.ResourceID = srn.ResourceID
+JOIN v_UpdateScanStatus scan ON s.ResourceID = scan.ResourceID
+JOIN v_GS_WORKSTATION_STATUS wsStatus ON s.ResourceID = wsStatus.ResourceID
+JOIN v_GS_OPERATING_SYSTEM ON s.ResourceID = v_GS_OPERATING_SYSTEM.ResourceID
+JOIN v_StateNames sn ON sn.StateID = scan.LastScanState AND sn.TopicType = 501

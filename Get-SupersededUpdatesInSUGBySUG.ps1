f704aa796ad6ef3469af7ce56887fc6d68e98797
Cody Mathis <cody.mathis123@gmail.com>
2021-01-19 04:57:13 -0800

    Account for SoftwareUpdateBundle

--- Get-SupersededUpdatesInSUGBySUG.ps1
+++ Get-SupersededUpdatesInSUGBySUG.ps1
@@ -2,7 +2,10 @@
 $SUG = Get-CMSoftwareUpdateGroup -Name 'Sug Name'
 $allInSUG = Get-CMSoftwareUpdate -UpdateGroup $SUG
 $updatesSugSupersedes = foreach ($Updates in $allInSUG) {
+    # Third party updates usually are not bundles
     ([xml]$Updates.sdmpackagexml).DesiredConfigurationDigest.SoftwareUpdate.SupersededUpdates.SoftwareUpdateReference.LogicalName
+    # Many MS Updates are bundles
+    ([xml]$Updates.sdmpackagexml).DesiredConfigurationDigest.SoftwareUpdateBundle.SupersededUpdates.SoftwareUpdateReference.LogicalName
 }
 $supersededUpdatesToRemove = $allInSUG.Where( { [string]::Format("SUM_{0}", $_.CI_UniqueID) -in $updatesSugSupersedes })
 $supersededUpdatesToRemove.LocalizedDisplayName

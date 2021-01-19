b4aa6d62d4de412dc81e9e9a0a859a64ec23fe09
Cody Mathis <cody@patchmypc.com>
2021-01-19 15:01:47 -0500

    switch to using GetElementsByTagName

--- Get-SupersededUpdatesInSUGBySUG.ps1
+++ Get-SupersededUpdatesInSUGBySUG.ps1
@@ -1,11 +1,10 @@
+param(
+    $SoftwareUpdateGroupName
+)
 # return a list of updates which are superseded by another update in the same SUG
-$SUG = Get-CMSoftwareUpdateGroup -Name 'Sug Name'
+$SUG = Get-CMSoftwareUpdateGroup -Name $SoftwareUpdateGroupName
 $allInSUG = Get-CMSoftwareUpdate -UpdateGroup $SUG
 $updatesSugSupersedes = foreach ($Updates in $allInSUG) {
-    # Third party updates usually are not bundles
-    ([xml]$Updates.sdmpackagexml).DesiredConfigurationDigest.SoftwareUpdate.SupersededUpdates.SoftwareUpdateReference.LogicalName
-    # Many MS Updates are bundles
-    ([xml]$Updates.sdmpackagexml).DesiredConfigurationDigest.SoftwareUpdateBundle.SupersededUpdates.SoftwareUpdateReference.LogicalName
+    ([xml]$Updates.sdmpackagexml).GetElementsByTagName("SupersededUpdates").SoftwareUpdateReference.LogicalName
 }
-$supersededUpdatesToRemove = $allInSUG.Where( { [string]::Format("SUM_{0}", $_.CI_UniqueID) -in $updatesSugSupersedes })
-$supersededUpdatesToRemove.LocalizedDisplayName
+return $allInSUG.Where( { [string]::Format("SUM_{0}", $_.CI_UniqueID) -in $updatesSugSupersedes })
